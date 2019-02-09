69211f69bbe7c407e9ab13c904f25d4365cff4f4
Kashif <kashifkhan.public@gmail.com>
2019-02-09 17:34:06 +0000

    Working version2

--- dre-to-dim.py
+++ dre-to-dim.py
@@ -1,9 +1,75 @@
 import sys
 
+def read_from_file_simple(path):
+    """
+    Read line by line from a file
+    :param path: 
+    :return: 
+    """
+    with open(path) as f:
+        data = f.readlines()
+    data = [x.strip() for x in data]
+    return data
+
+def write_to_file_simple(path, data):
+    """
+    Write to file using raw strings
+    - Print line by line using new line separators
+    :param path: 
+    :param data: 
+    :return: 
+    """
+    with open(path, 'w') as outfile:
+        for datum in data:
+            out = datum
+            if datum != data[-1]:
+                out = out + "\n"
+            outfile.write(out)
+
+
 if __name__ == "__main__":
     if(len(sys.argv) != 3):
         print "Error. \n Incorrect number of arguments. \n Usage 'python {drefile}.dre {dimacsfile}'. \n"
         exit()
-        
-    #print("\n".join(sys.argv))
-    print(sys.argv[1])
+
+    # Init
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+    outData = []
+    
+    # Read data
+    data = read_from_file_simple(inFile)
+    
+    # Process input data
+    header = data.pop(0)
+    header_array = header.split(" ")
+    
+    # Nodes
+    nodes = header_array[1][2:]
+
+    # Edges
+    edges = 0
+    for i, v in enumerate(data):
+        edges += len(v.split(" ")) - 1
+
+
+    # Process output data
+    outHeader = "p edge "+ str(nodes) + " " + str(edges)
+    outData.append(outHeader)
+
+    # Rows
+    for i, v in enumerate(data):
+        line = v.split(" ")
+        node = line.pop(0)[:-1]
+        lines = []
+
+        for j, v2 in enumerate(line):
+            newLine = "e "+ str(node) + " " + str(v2)
+            newLine = newLine.replace('.', '')
+
+            if(i == len(data) - 1):
+                continue
+
+            outData.append(newLine)
+    
+    write_to_file_simple(outFile, outData)
