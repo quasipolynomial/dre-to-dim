2a61a42b3eb20495cd3b09776bdb22b2eb7886d2
Kashif <kashifkhan.public@gmail.com>
2019-02-10 13:54:49 +0000

    Add bulk processing

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Python 2.7
 
 # Usage
 python dre_to_dim.py {input_dreadnaut_filepath.dre} {output_dimacs_filepath}
+
+OR
+
+python dre_to_dim_bulk.py {input_dreadnaut_folderpath} {output_dimacs_folderpath}
+
--- dre_to_dim_bulk.py
+++ dre_to_dim_bulk.py
@@ -1,9 +1,35 @@
 import sys
+import os
 from dre_to_dim import DreToDim
+
+
+def load_files(inFolder):
+    files = []
+    for fileName in run_command('ls -v ' + inFolder):
+        files.append(fileName)
+    return files
+
+
+def run_command(command):
+    """
+    Run a command and return a list
+    Good for single lined commands
+    :param command: 
+    :return: 
+    """
+    p = os.popen(command, "r")
+    out = []
+    while 1:
+        line = p.readline()
+        if not line:
+            break
+        out.append(line.rstrip())
+    return out
+
 
 if __name__ == "__main__":
     if(len(sys.argv) != 3):
-        print("Error. \n Incorrect number of arguments. \n Usage 'python dre_to_drim_bulk.py {dreFolder} {dimacsFolder}'. \n")
+        print ("Error. \n Incorrect number of arguments. \n Usage 'python dre_to_drim_bulk.py {dreFolder} {dimacsFolder}'. \n")
         exit()
 
     # Init
@@ -11,3 +37,9 @@ if __name__ == "__main__":
     outFolder = sys.argv[2]
 
     x = DreToDim()
+    files = load_files(inFolder)
+    for i, fileName in enumerate(files):
+        inFileName = inFolder + "/" + fileName
+        outFileName = outFolder + "/" + fileName.replace('.dre', '')
+        x.process(inFileName, outFileName)
+
